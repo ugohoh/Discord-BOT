bbfd8a8c4f5b038b4545849496842032ea8b40b2
Hugo Bringout <hugo.bringout@epita.fr>
2022-09-21 20:55:47 +0200

    until ban without admin

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
 import discord
+from random import randrange
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+intents.presences = True
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True, # Commands aren't case-sensitive
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = XXX  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +23,44 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author)
+
+@bot.command()
+async def d6(ctx):
+    await ctx.send(randrange(6))
+
+@bot.event
+async def on_message(message):
+    if message.content =="Salut tout le monde":
+        await message.channel.send("Salut tout seul @"+"<@" + str(message.author.id) + ">")
+    await bot.process_commands(message)
+
+@bot.command()
+async def count(ctx):
+    online=0
+    idle=0
+    off=0
+    for guild in bot.guilds:
+        for member in guild.members:
+            if str(member.status)=="online":
+                online=online+1
+            elif str(member.status)=="offline":
+                off=off+1
+            elif str(member.status)=="idle":
+                idle=idle+1
+    await ctx.send(str(online) +" members are online, "+ str(idle) +" are idle and "+ str(off) +" are off")
+
+@bot.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    if str(member.name)!="Hugo La Bringue":
+        await member.ban(reason = reason)
+
+@bot.command()
+async def admin(ctx, member : discord.Member):
+    print(member.name)
+
+token = "XXXX"
 bot.run(token)  # Starts the bot
